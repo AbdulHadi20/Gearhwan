3dfe6b10ec33f9a7bbed53a71cee24ca3ccc1fbe
AbdulHadi <abdulhadi2003m@gmail.com>
2025-06-02 22:52:11 +0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,21 @@
 import { defineConfig } from 'astro/config';
 import clerk from '@clerk/astro';
 import vercel from '@astrojs/vercel';
-
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
+
 
 
 // https://astro.build/config
+
 export default defineConfig({
-  integrations: [clerk(),react(),tailwind()],
+
+  integrations: [clerk(),react()],
   adapter: vercel(),
   output: 'server',
+  
+  vite: {
+    plugins: [tailwindcss()]
+  }
 
 });
